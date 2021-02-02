3b44b988ff1bf7d28e1f9ed3652a6c73bcf2046f
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-02-02 12:35:04 -0600

    added problems 2 and 3 from JS to HTML Exercise

--- js/external.js
+++ js/external.js
@@ -1,2 +1,13 @@
 console.log("Hello from External Javascript");
 alert("Welcome to my Website!");
+
+var favoriteColor = prompt ('What is your favorite color?');
+console.log('Great, my favorite color is ' + favoriteColor + ' too!');
+
+var movieRental = prompt ("How many nights would you like to rent the movie for?");
+console.log('Movie has been rented for ' + movieRental + ' nights!');
+alert('Great, you can rent the movie for ' + movieRental + ' nights!');
+
+var googlePay = prompt ('How many hours did you work at Google this week?');
+console.log('Employee worked ' + googlePay + ' hours.');
+alert('Great, so you will get paid ' + '$' + googlePay*400 + ' dollars!')
