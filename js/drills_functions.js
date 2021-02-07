bce98f776081bd9c3d786502bf8be0690f14fd35
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-02-06 21:04:00 -0600

    Drills Set Up

---
+++ js/drills_functions.js
@@ -0,0 +1,20 @@
+// Make a function called returnTwo() that returns the number 2 when called
+// Test this function with console.log(returnTwo())
+function returnTwo() {
+    return (2);
+}
+console.log(returnTwo());
+
+//     Make a function called sayHowdy() which console.logs the string “Howdy!”
+// Test this function by directly calling sayHowdy()
+function sayHowdy('Howdy!') {
+}
+console.log(sayHowdy);
+
+// Remember this function does not need a defined return value
+// Make a function called returnName() that returns the string of your name
+// Test this function with console.log(returnName())
+//     Make a function called addThree() which takes in a number input and returns the number plus 3.
+// Test this function with console.log(addThree(5))
+//     Make a function called sayString() which returns the string input passed in.
+// Test this function with console.log(sayString('codeup'))
