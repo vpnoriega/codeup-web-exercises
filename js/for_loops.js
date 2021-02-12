ea532c92a93c95cb1c38ea15bf22ffb5d2221aa1
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-02-12 14:20:05 -0600

    Number 4 Complete

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,12 +8,21 @@
 // showMultiplicationTable(7)
 
 //Number 3:
-for (var i = 1; i <= 10; i++) {
-    num = (Math.floor(Math.random() * (201 - 20 + 1) + 20))
-    if (num % 2 === 0)
-        console.log(num + " is even.")
-    else console.log(num + " is odd.")
+// for (var i = 1; i <= 10; i++) {
+//     num = (Math.floor(Math.random() * (201 - 20 + 1) + 20))
+//     if (num % 2 === 0)
+//         console.log(num + " is even.")
+//     else console.log(num + " is odd.")
+// }
+
+// Number 4:
+for (var O = 1; O <10; O++) { // O = Outerloop prints # of rows
+    var output = ""; // actual string because it's not a legit number
+    for (var I = 1; I<=O; I++){ // I = innerloop prints how many times to repeat the row number
+        output = output + O; // empty string + outerloop
+    }
+    console.log (output);
 }
 
-
+//Number 5:
 
