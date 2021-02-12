7b8ef51756e7178d6e07e8c742960a7bd4168ba4
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-02-12 13:43:43 -0600

    Problem 3 Complete

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,8 +1,19 @@
-function showMultiplicationTable (x){
-    for (var i =1; i<=10; i++) {
-        console.log(x +" x " + i + " = " + (x * i));
-    }
+//Number 1: Create File
+//Number 2:
+// function showMultiplicationTable (x){
+//     for (var i =1; i<=10; i++) {
+//         console.log(x +" x " + i + " = " + (x * i));
+//     }
+// }
+// showMultiplicationTable(7)
+
+//Number 3:
+for (var i = 1; i <= 10; i++) {
+    num = (Math.floor(Math.random() * (201 - 20 + 1) + 20))
+    if (num % 2 === 0)
+        console.log(num + " is even.")
+    else console.log(num + " is odd.")
 }
-showMultiplicationTable(7)
+
 
 
