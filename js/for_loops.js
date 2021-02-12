bbbfdb9f22b675c0524b7c1050594b6a9549a60d
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-02-12 15:28:47 -0600

    For Loops Section Complete

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,19 +1,19 @@
 //Number 1: Create File
 //Number 2:
-// function showMultiplicationTable (x){
-//     for (var i =1; i<=10; i++) {
-//         console.log(x +" x " + i + " = " + (x * i));
-//     }
-// }
-// showMultiplicationTable(7)
+function showMultiplicationTable (x){
+    for (var i =1; i<=10; i++) {
+        console.log(x +" x " + i + " = " + (x * i));
+    }
+}
+showMultiplicationTable(7)
 
 //Number 3:
-// for (var i = 1; i <= 10; i++) {
-//     num = (Math.floor(Math.random() * (201 - 20 + 1) + 20))
-//     if (num % 2 === 0)
-//         console.log(num + " is even.")
-//     else console.log(num + " is odd.")
-// }
+for (var i = 1; i <= 10; i++) {
+    num = (Math.floor(Math.random() * (201 - 20 + 1) + 20))
+    if (num % 2 === 0)
+        console.log(num + " is even.")
+    else console.log(num + " is odd.")
+}
 
 // Number 4:
 for (var O = 1; O <10; O++) { // O = Outerloop prints # of rows
@@ -24,5 +24,7 @@ for (var O = 1; O <10; O++) { // O = Outerloop prints # of rows
     console.log (output);
 }
 
-//Number 5:
-
+//Number 5
+for (var i = 100; i >= 5; i-=5) {
+    console.log(i);
+}
