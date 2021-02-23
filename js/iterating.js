5794c07774a413354740cb90ee668611512e7710
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-02-23 13:41:47 -0600

    First Attempt Completed

--- js/iterating.js
+++ js/iterating.js
@@ -6,18 +6,27 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-var names = ['Nathan', 'Sergio' , 'Vanessa', 'Sarah' ]
+
+    var names = ['Nathan', 'Sergio' , 'Vanessa', 'Sarah' ]
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-console.log()
+
+    console.log(names.length)
+
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
 
     /**
      * TODO:
@@ -25,10 +34,19 @@ console.log()
      * array.
      */
 
+    for (var i = 0; i < names.length; i++){
+        console.log(names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+
+names.forEach(function(name){
+    console.log(names);
+})
+
 
     /**
      * TODO:
@@ -43,5 +61,35 @@ console.log()
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+    //first
+function first (array){
+    return (array[0])
+    }
+    var numbers = [1,2,3,4,5]
+console.log(first(numbers))
+
+
+//second
+    function second (array){
+        return (array[1])
+    }
+    var numbers = [1,2,3,4,5]
+    console.log(second(numbers))
+
+
+//last
+    function last (array){
+        return (array[4])
+    }
+    var numbers = [1,2,3,4,5]
+    console.log(last(numbers))
+
+// alternatively without the variable:
+function last (array){
+    return (array[4])
+}
+console.log(last([1,2,3,4,5]))
+
 
 })();
