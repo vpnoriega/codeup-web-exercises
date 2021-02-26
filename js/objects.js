bc182c5b9a0b6f4a8d6a914d741c2e80b6a33f27
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-02-25 22:26:48 -0600

    Exercise Complete - Working on Bonus Q

--- js/objects.js
+++ js/objects.js
@@ -13,8 +13,8 @@
      */
 
     var person = {}
-        person.firstName = "Vanessa";
-        person.lastName = "Noriega";
+    person.firstName = "Vanessa";
+    person.lastName = "Noriega";
 
     console.log(person);
 
@@ -28,9 +28,10 @@
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
 
-function sayHello(){
-    return "Hello " + person.firstName +" " + person.lastName + "!"
+    function sayHello() {
+        return "Hello " + person.firstName + " " + person.lastName + "!"
     }
+
     console.log(sayHello())
 
 
@@ -80,6 +81,45 @@ function sayHello(){
      * > console.log(books[0].author.lastName) // "Adams"
      */
 
+    var books = [
+        {
+            title: 'The Outsiders',
+            author: {
+                firstName: "S.E.",
+                lastName: "Hinton"
+            }
+        },
+        {
+            title: 'To Kill A Mockingbird',
+            author: {
+                firstName: "Harper",
+                lastName: "Lee"
+            }
+        },
+        {
+            title: 'The Great Gatsby',
+            author: {
+                firstName: "F. Scott",
+                lastName: "Fitzgerald"
+            }
+        },
+        {
+            title: 'Things Fall Apart',
+            author: {
+                firstName: "Chinua",
+                lastName: "Achebe"
+            }
+        },
+        {
+            title: 'The Color Purple',
+            author: {
+                firstName: "Alice",
+                lastName: "Walker"
+            }
+        }
+    ];
+
+
     /**
      * TODO:
      * Loop through the books array and output the following information about
@@ -105,6 +145,17 @@ function sayHello(){
      *      ...
      */
 
+    for (var i = 0; i < books.length; i++) {
+        console.log("Book # " + (i + 1) + '\n' + "Title: " + books[i].title + '\n' + "Author: " + books[i].author.firstName + " " + books[i].author.lastName);
+    }
+
+// The initial set up for the console log:
+// books[i].title + ", written by: " + books[i].author.firstName + " " + books[i].author.lastName)
+
+// Before the for loop, I did this for each book:
+// console.log(books[0].title + ", written by: " + books[0].author.firstName + " " + books[0].author.lastName);
+
+
     /**
      * Bonus:
      * - Create a function named `createBook` that accepts a title and author
@@ -115,5 +166,32 @@ function sayHello(){
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
      */
+
+    function createBook(title,author){
+        var book = {}
+        book.title = title;
+        book.author = author;
+        return book
+    }
+
+//     function createBook(title, authorFirst, authorLast) {
+//         var book = [
+//             {
+//                 title: title,
+//                 author: {
+//                     firstName: authorFirst,
+//                     lastName: authorLast
+//                 }
+//             }]
+//         return book
+// }
+console.log(createBook('x','y'))
+
+    function showBookInfo(book){
+        for (var i = 0; i < book.length; i++) {
+            console.log("Book # " + (i + 1) + '\n' + "Title: " + book[i].title + '\n' + "Author: " + book[i].author.firstName + " " + book[i].author.lastName);
+        }
+    }
+    console.log(showBookInfo(books))
 
 })();
