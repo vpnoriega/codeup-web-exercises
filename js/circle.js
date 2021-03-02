6fdae7217bfaeae127f41464ec0055b312c7750c
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-03-01 22:32:48 -0600

    updated last problem

--- js/circle.js
+++ js/circle.js
@@ -34,25 +34,8 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-    var circle = {
-        radius: 5,
+    circle.radius = 5
 
-        getArea: function () {
-            return Math.PI * (Math.pow(this.radius,2))
-        },
-
-
-        logInfo: function (doRounding) {
-            // TODO: complete this method.
-            if (doRounding === true) {
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea()));
-            } else {
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
-            }
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-        }
-    };
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
