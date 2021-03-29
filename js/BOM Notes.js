1fa4fa36ee39ba7ed066e33d00a160027b429b05
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-03-28 23:05:06 -0500

    Added Notes from Curriculum

---
+++ js/BOM Notes.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/* setInterval() & clearInterval */
+
+// this code will produce a console log every second
+// when count >= max, the interval is cancelled, and the logging will stop
+
+var count = 0;
+var max = 10;
+var interval = 1000; // interval time in milliseconds
+
+var intervalId = setInterval(function () {
+    if (count >= max) {
+        clearInterval(intervalId);
+        console.log('All done');
+    } else {
+        count++;
+        console.log('Repeating this line ' + count);
+    }
+}, interval);
+
+
+/* setTimeout() */
+
+var delay = 5000; // delay time in milliseconds
+
+var timeoutId = setTimeout(function () {
+    alert('Here is a delayed hello!');
+}, delay);
+
+// to cancel the timeout, you can call
+// clearTimeout(timeoutId);
+// prior to the delay expiring
