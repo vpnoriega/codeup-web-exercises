87b69f5f552f930ac2d481bc496e5f55338aac3d
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-01 15:16:24 -0500

    Sending to GIT

--- js/DOM-student.js
+++ js/DOM-student.js
@@ -44,25 +44,30 @@ function locateElements(element){
 // TODO: Howell's doesn't sound like the name you'd give your hat shop - let's change the innerText of that in our navigation bar to your own name.
 
     var navName = document.getElementsByClassName('navbar-brand');
-    console.log(navName.innerText);
-    navName.innerText = "Noriega's"
-    console.log(navName.innerText)
+    console.log(navName[0].innerText);
+    navName[0].innerText = "Noriega's"
+    console.log(navName[0].innerText)
 
 // TODO: There's only one ordered list on the page - can you get into that ordered list and use innerHTML to change the list items within that ordered list to what you think the top sellers were?
 
 var listItems = document.getElementsByTagName('ol');
-    console.log(listItems.innerHTML);
-    listItems.innerHTML="funny hat, beanie, santa hat";
+    console.log(listItems[0].innerHTML);
+    listItems[0].innerHTML="1. funny hat 2. beanie 3. santa hat";
 console.log(listItems.innerHTML);
 
 // TODO (BONUS): I have two "special-offer" classes used in my table - grab those classes by name, store them into a variable, and then use a loop to replace the innerHTML. The new table data should have strings that let our customers know that "New discounts are coming soon!"
 
+// var offerReplacement = document.getElementsByClassName("special-offer")
+// for (var i=0; i<offerReplacement; i+=){
+//     offerReplacement.innerHTML="New discounts are coming soon!"
+// }
 
 
-// attributes
+// attributes ** I did not understand this at all **
 
 // TODO: Bringing in Bootstrap components, sometimes we can forget to change example or demo related text - one of my ids in the HTML is set to "navbarTogglerDemo02". I don't think we need that ID at all, can you remove that attribute for me? Start by storing it in a variable!
-
+var deleteAtt = document.getElementById("navbarTogglerDemo02");
+deleteAtt.removeAttribute("id")
 
 
 // TODO: Uh oh - actually there was a BUTTON that referred to 'navbarTogglerDemo02' that is right above it in our HTML structure! Let's add an ID back to the variable you used above that makes more sense (perhaps "navbar-collapser"?)
@@ -77,6 +82,13 @@ console.log(listItems.innerHTML);
 
 // TODO: I've embedded a style sheet that adjusts color for the body, a class called "main-content", and an ID of "top-header". Target the three of these elements and set your own colors as you'd like to each of the three I've mentioned.
 
+var bodyColor = document.getElementsByTagName('body')[0];
+bodyColor.style['background-color'] = "grey";
 
+var mainContent = document.getElementsByClassName('main-content');
+mainContent[0].style.color="rebeccapurple";
+
+var topHeader = document.getElementById('top-header');
+topHeader.style.color="aqua";
 
 // TODO (BONUS): That default bootstrap font is nice, but we've been adding our own fonts to thing for a couple of weeks now! Add a font-family of some kind that you'd like it to adjust to.
