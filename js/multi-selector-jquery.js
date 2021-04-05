7bc10ef516f0e505ffcbd334c365d428cc79cfcf
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-05 15:40:53 -0500

    mini group presentation

---
+++ js/multi-selector-jquery.js
@@ -0,0 +1,7 @@
+// $('.important, p').css('background-color', 'rebeccapurple');
+
+// $('h2,h1').css('background-color','cornflowerblue')
+
+// var contents = $('.not-important,p').html();
+// alert(contents);
+
