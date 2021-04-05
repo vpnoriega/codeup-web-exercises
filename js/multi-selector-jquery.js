b31426d15f9f139813594f9579a0bc8d722a75f5
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-05 16:24:57 -0500

    added all selector

--- js/multi-selector-jquery.js
+++ js/multi-selector-jquery.js
@@ -1,7 +1,10 @@
-// $('.important, p').css('background-color', 'rebeccapurple');
+$('.important, p').css('background-color', 'rebeccapurple');
 
-// $('h2,h1').css('background-color','cornflowerblue')
+$('h2,h1').css('color','cornflowerblue')
 
-// var contents = $('.not-important,p').html();
-// alert(contents);
+var contents = $('.not-important,p').html();
+alert(contents);
 
+console.log(contents);
+
+$('*').css('border', '1px solid #F00');
