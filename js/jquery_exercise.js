e4c942eae014459071c7e7b9a2344d5f6f8777da
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-07 18:28:30 -0500

    feat: Jquery Mouse Events Exercise complete with Notes

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -10,7 +10,7 @@
 
 
 /** create a border around all elements with the class codeup that is 1 pixel wide and red. */
-$('.codeup').css('border','1px solid red');
+// $('.codeup').css('border','1px solid red');
 
 //or like an object:
 // $('.codeup').css({'border':'1px solid red'});
@@ -20,32 +20,57 @@ $('.codeup').css('border','1px solid red');
 
 
 /** Set the font-size of all li elements to 20px.*/
-$('li').css({'font-size':'20px', 'color':'blue'});
+// $('li').css({'font-size':'20px', 'color':'blue'});
 
 // ?? Not sure why this didn't work like the example above:
 // $('li').css('font','blue 20px');
 
 
 /** Craft selectors that highlight all the h1, p, and li elements.*/
-$('h1, p, li').css({'backgroundColor':'pink'})
+// $('h1, p, li').css({'backgroundColor':'pink'})
 
 
+/***** Jquery Mouse Events *****/
 
-//example with outside function
-// function h1Click() {
-//     $(this).css('background', 'limegreen');
-// }
-//
-// $('h1').click(h1Click);
-//
-// //example with multiple functions using BOTH outside and inside
-// function hovering(e) {
-//     console.log('enter');
-//     $(e.target).css('color', 'red');
-// }
-//
-// $('li').hover(hovering, function() {
-//     console.log('leave');
-//     $(this).css('color', 'black');
+
+/** change the background color of an h1 element when clicked. */
+//example with named function as a callback function:
+function h1Click() {
+    $(this).css('background', 'cornflowerblue');
+}
+// ** 'this' is referring to the element being called below:
+$('h1').click(h1Click);
+
+
+/***** Make all paragraphs have a font size of 18px when they are double clicked *****/
+//example with anonymous function as a callback function:
+// $('p').dblclick(function(e) {
+//     $(e.target).css('font-size', '18px');
 // });
+
+//refactored with named function as a callback function:
+function dosClick(e){
+    $(e.target).css('font-size', '18px');
+}
+$('p').dblclick(dosClick);
+
+
+/***** Set all li text color to red when the mouse is hovering; reset to black when it is not*****/
+// hover handler combines 2 event handlers (mouseenter and mouseleave), so you need 2 callback functions to work together:
+
+function hovering(e) {
+    console.log('enter');
+    $(e.target).css('color', 'red');
+}
+$('li').hover(hovering, function() {
+    console.log('leave');
+    $(this).css('color', 'black');
+});
+
+// ** Syntax for hover:
+// $( selector ).hover( handlerIn, handlerOut)
+
+// ** Syntax for attaching both handlers separately:
+// $( selector ).mouseenter(handlerIn)
+// $( selector ).mouseleave(handlerOut);
 
