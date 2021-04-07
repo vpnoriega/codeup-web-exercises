1627a863f930a2a30c4b5d5e2de35b562a9e87d8
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-06 20:50:03 -0500

    adding files

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -1,5 +1,19 @@
-// window.onload = function() {
-//     alert( 'The page has finished loading!' );
-// }
-var contents = $('#content').html();
-alert(contents);
+'use strict';
+
+//example with outside function
+function h1Click() {
+    $(this).css('background', 'limegreen');
+}
+
+$('h1').click(h1Click);
+
+//example with multiple functions using BOTH outside and inside
+function hovering(e) {
+    console.log('enter');
+    $(e.target).css('color', 'red');
+}
+
+$('li').hover(hovering, function() {
+    console.log('leave');
+    $(this).css('color', 'black');
+});
