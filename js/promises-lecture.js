987fc9298f7620e840f6876e0005edcae3f7c3a5
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-19 15:04:49 -0500

    Additional Notes

--- js/promises-lecture.js
+++ js/promises-lecture.js
@@ -4,14 +4,22 @@
  *        USING FETCH WITH PROMISES
  ******************************************** */
 
-var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
+//var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
 // return of fetch request is a Promise object
 
 //TODO: console log the results of the above Promise. What is the result?
 
+//console.log(pokemonAPI)
+
 //TODO: add a method that runs if the Promise resolves successfully
 
+// pokemonAPI.then((response)=>{
+//     response.json().then((jsonResponseData)=>console.log(jsonResponseData));
+// });
+
 //TODO: add a method that runs if the Promise fails
+
+//pokemonAPI.catch(reason => console.log(reason));
 
 //TODO: In the callback function of the .then method, parse the response into JSON
 
@@ -24,6 +32,11 @@ var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
 //  the first returned response.
 //    Taking a look at this second return, what are the results? What is the shape of our data?
 
+// var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon')
+//     .then((response)=>{return response.json()})
+//     .then((jsonData)=> {return jsonData.results})
+//     .then((results)=>results.forEach(result=>console.log(result.name)));
+
 // TODO: Finally, chain another .then method that would log all of the name properties of the
 //  returned pokemon.
 // BONUS: Is there a way for us to clean up our code?
@@ -32,8 +45,31 @@ var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
 
 // TODO: Using Promises, make a fetch request to the Star Wars API
 
+// let starWarsAPI = fetch("https://swapi.dev/api/films")
+//     .then((response)=>{return response.json()})
+//     .then((jsonData)=>{
+//         console.log(jsonData)
+//         return jsonData.results;
+//     })
+//     .then((results)=>{
+//         results.forEach((film) =>console.log(`${film.title} ${film.episode_id}`))
+// });
+
+// console.log(starWarsAPI)
+
 // TODO: Use Promise chaining to console log the json response
 
 // TODO: chain another method that iterates through the results array and console logs the names
 
 // TODO: Demonstrate Promise.all and Promise.race
+
+let starWarsAPI = fetch("https://swapi.dev/api/films")
+var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon')
+
+// Promise.all([starWarsAPI,pokemonAPI]).then((responses)=>{
+//     console.log(responses);
+// });
+
+Promise.race([starWarsAPI,pokemonAPI]).then((response)=>{
+    console.log(response);
+})
